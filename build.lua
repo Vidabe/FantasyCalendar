e8f1a7b478e54116b81328199d9548b5ef21361f
Ben <b.vitecek@gmx.at>
2021-08-13 17:57:15 +0200

    Create "build.lua"

---
+++ build.lua
@@ -0,0 +1,9 @@
+module = "another_fantasycalendar"
+
+typesetdemofiles = { "example-calendar-1.tex" }
+
+typesetfiles = { "typesetfiles" }
+
+sourcefiles = { "*.code.tex", "*.cls", "*.sty"}
+installfiles = sourcefiles
+
